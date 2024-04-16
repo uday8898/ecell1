ffb023b307c82150ba85bf47acc75ac08645c43c
UDAY GUPTA <91193540+uday8898@users.noreply.github.com>
2024-04-16 16:47:39 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ export const Navbar = () => {
                         <li><a className="nav-link scrollto" href="#clients">Partners</a></li>
                         <li><a className="nav-link scrollto" href="#about">About</a></li>
                         <li><a className="nav-link scrollto" href="#services">Genie</a></li>
-                        <li><a className="nav-link scrollto " href="#portfolio">Gallery</a></li>
+                        <li><a className="nav-link scrollto " href="#gallery">Gallery</a></li>
                         <li><a className="nav-link scrollto " href="#testimonials">StartUps</a></li>
                         <li><a className="nav-link scrollto" href="#team">Team</a></li>
                         <li><a className="nav-link scrollto" href="#mentor">Mentors</a></li>
